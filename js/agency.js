fe82cfa27d13190365e390f8551e9d88c5ee0ba7
Drew Price <drewprice@users.noreply.github.com>
2015-10-17 14:25:08 -0400

    Reinstall agency

--- js/agency.js
+++ js/agency.js
@@ -16,11 +16,9 @@ $(function() {
 });
 
 // Highlight the top nav as scrolling occurs
-if ( window.location.pathname != "/blog/" ) {
-  $('body').scrollspy({
-      target: '.navbar-fixed-top'
-  })
-}
+$('body').scrollspy({
+    target: '.navbar-fixed-top'
+})
 
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
